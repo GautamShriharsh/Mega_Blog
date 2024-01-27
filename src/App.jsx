4f3d335c6686e2a901628d93b3fa7d6277b4b393
Shriharsh <shriharshgautam7@gmail.com>
2024-01-27 22:11:03 +0530

    created useEffect that checks if user is logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-
+import React,{ useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout} from './store/authSlice'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+ const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      }
+      else {s
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
 
-  return (
-    <>
-      <h1>MegaBlog Project</h1>
-    </>
-  )
+
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>Testing</div>
+  ) : (null)
 }
 
 export default App
