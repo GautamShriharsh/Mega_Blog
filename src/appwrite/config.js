675ca54765b6aaad2c76a30329852759cd3ebb4e
Shriharsh <shriharshgautam7@gmail.com>
2024-01-27 13:15:42 +0530

    Created deleteFile function

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -96,6 +96,36 @@ export class Service {
     }
   }
 
+  // file upload service
+
+  async uploadFile(file) {
+     try {
+        return await this.bucket.createFile(
+            conf.appwriteBucketId,
+                    ID.unique(),
+                    file,
+
+
+        )
+     } catch (error) {
+        console.log("Appwrite service :: uploadFile :: error", error)
+        return false;
+     }
+  }
+
+  async deleteFile(fileId) {
+   try {
+     await this.bucket.deleteFile(
+        conf.appwriteBucketId,
+        fileId,
+    )
+    return true;
+   } catch (error) {
+    console.log("Appwrite service :: deleteFile :: error", error)
+    return false;
+   }
+  }
+
 
 }
 
