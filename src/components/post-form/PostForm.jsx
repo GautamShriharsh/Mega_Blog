3f5649f8bd3e721b04d25a41963ab7d35957cbaa
Shriharsh <shriharshgautam7@gmail.com>
2024-02-02 22:49:27 +0530

    regExp for slugTransform

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -3,7 +3,7 @@ import {useForm} from 'react-hook-form'
 import {Button, Input, Select, RTE} from '../index'
 import appwriteService from '../../appwrite/config'
 import { useNavigate } from 'react-router-dom'
-import { UseSelector, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 function PostForm() {
    const {register, handleSubmit, watch, setValue, control, getValues} = useForm({
@@ -12,8 +12,11 @@ function PostForm() {
         slug: post?.slug || '',
         content: post?.content || '',
         status: post?.status || 'active',
-    }
+    },
    })
+
+   const navigate = useNavigate()
+   const userData = useSelector((state) => state.userData)
 
    const submit = async (data) => {
     if (post) {
@@ -22,23 +25,51 @@ function PostForm() {
         if (file) {
             appwriteService.deleteFile(post.featuredImage)
         }
+
         const dbPost = await appwriteService.updatePost(post.$id, {
             ...data,
             featuredImage: file ? file.$id : undefined,
         })
+        
         if (dbPost) {
             navigate(`/post/${dbPost.$id}`)
+        } 
+      }
+
+    else {
+         const file = await appwriteService.uploadFile(data.image[0])
+
+           if (file) {
+            const fileId = file.$id
+            data.featuredImage = fileId
+            const dbPost = await appwriteService.createPost({
+                ...data,
+                userId: userData.$id,
+            })
+            if (dbPost) {
+                navigate(`/post/${dbPost.$id}`)
+            }
+           } 
         }
     }
-   }
+   
 
-   const navigate = useNavigate()
-   const userData = useSelector((state) => state.userData)
+     const slugTransform = useCallback((value) => {
+      if (value && typeof value === 'string') {
+        return value
+           .trim()
+           .toLowerCase()
+           .replace(/[^\w\s-]/g, '') // Remove non-word characters except whitespace and dash
+            .replace(/[\s]+/g, '-') // Replace whitespace sequences with a single dash
+            .replace(/^-+/g, '') // Remove leading dashes
+            .replace(/-+$/g, ''); // Remove trailing dashes
+      }
+     }, [])
 
   
     return (
     <div>
-      
+      PostForm
     </div>
   )
 }
